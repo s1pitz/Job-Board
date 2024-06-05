0be8b3cee8881b5eaa3546b73a5ad275e37d7820
s1pitz <richard.arthur@binus.ac.id>
2024-06-05 21:32:25 +0700

    different UI fro user login

--- resources/views/UserAuth/partial/base.blade.php
+++ resources/views/UserAuth/partial/base.blade.php
@@ -10,11 +10,11 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     @vite('resources/css/app.css')
 </head>
-<body class="">
+<body class="bg-blue-700" style="background-color: #2264dd">
 
     @include('UserAuth.partial.header')
 
-    <div class="container p-4">
+    <div class="mx-auto container flex justify-center items-center h-screen">
         @yield('content')
     </div>
 
